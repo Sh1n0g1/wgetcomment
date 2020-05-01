573ef22118f95ca39b4c9cbec9f8df92b0dc6634
Sh1n0g1 <shinogi-s@macnica.net>
2020-05-02 07:09:06 +0800

    Update wgetcomment.py
    Add capability to scan javascript, css comments.

--- wgetcomment.py
+++ wgetcomment.py
@@ -19,12 +19,38 @@ except:
   
 if result.status_code >= 400:
   print("An error is happening when accessing the URL:%s\nStatus code is %s." % (url, result.status_code ))
+  exit()
+  
 contents=result.text
 
 #3.Extract the comment
 #html comment <!--  -->
-comments=re.findall("<!--(.*?)-->", contents, flags=re.DOTALL)
+htmlcomments=re.findall("<!--(.*?)-->", contents, flags=re.DOTALL)
+
+#js <script > </script> /* */, //
+javascripts=re.findall("<script[^>]*>(.*?)</script>", contents, flags=re.DOTALL)
+
+jscomments=[]
+for javascript in javascripts:
+  jscomments += re.findall("/\*(.*?)\*/", javascript, flags=re.DOTALL)
+  jscomments += re.findall("[^:'\"\\\\]//(.*)", javascript)
+
+#css /* */
+csss=re.findall("<style[^>]*>(.*?)</style>", contents, flags=re.DOTALL)
+csscomments=[];
+for css in csss:
+  csscomments+=re.findall("/\*(.*?)\*/", css, flags=re.DOTALL)
+
 
 #4.Print the comment
-for comment in comments:
-  print("[HTML]: " + comment)
+if htmlcomments:
+  for htmlcomment in htmlcomments:
+    print("[HTML]: " + htmlcomment.strip())
+
+if jscomments:
+  for jscomment in jscomments:
+    print("[JS  ]: " + jscomment.strip())
+
+if csscomments:  
+  for csscomment in csscomments:
+    print("[CSS ]: " + csscomment.strip())
