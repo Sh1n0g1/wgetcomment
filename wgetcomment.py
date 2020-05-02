1eeaef0cfbeaaf3abd8bdd71ce2debbe4ab14a58
Sh1n0g1 <shinogi-s@macnica.net>
2020-05-02 15:39:32 +0800

    Update wgetcomment.py

--- wgetcomment.py
+++ wgetcomment.py
@@ -1,35 +1,76 @@
-#! python3
+#!/usr/bin/env python3
 import sys
+import os
 import requests
 import re
+import csv
 
-#1.Get the URL from the command line argument
+USER_AGENT = 'wgetcomment 1.0'
+
+def analyze_comment(comment):
+  symbol_count=0;
+  for c in comment:
+    if (ord(c) > 0x20 and ord(c) < 0x30) or (ord(c) > 0x39 and ord(c) < 0x41) or (ord(c) > 0x5A and ord(c) < 0x61) or (ord(c) > 0x7A and ord(c) < 0x7F):
+      symbol_count+=1
+  
+  return symbol_count/ len(comment)
+  
+
+def output(type, comments):
+  if output.csvoutput:
+    try:
+      with open(output.filename, 'a', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+        for comment in comments:
+          output.counter +=1
+          
+          csvwriter.writerow([output.counter, type, analyze_comment(comment), comment])
+    except Exception as e:
+      print("[-] Can not access to the file \"%s\"\n    Error:%s" % (filename, e))
+      exit()
+  else:
+    for comment in comments:
+      output.counter +=1
+      print("[%s %3d]: %s" % (type.ljust(4), output.counter, comment.strip()))
+output.counter=0
+
+#.Get the URL from the command line argument
 if len(sys.argv) < 2:
-  print("Usage: wgetcomment <url>")
+  print("Usage: wgetcomment <url>", file=sys.stderr)
   exit()
   
 url=sys.argv[1]
-
+if len(sys.argv)==3:
+  output.csvoutput = True
+  output.filename = sys.argv[2]
+  if os.path.exists(output.filename):
+    print("[-] The file \"%s\" is already existing" % (output.filename))
+    exit()
+else:
+  output.csvoutput = False
+  filename = ""
+  
 #2.Fetch the contents from the URL
+print("[+] Accessing to %s" % url, file=sys.stderr)
 try:
-  result=requests.get(url)
-except:
-  print("An error is happening when accessing the URL:" + url)
+  response=requests.get(url, headers={'User-agent': USER_AGENT})
+except Exception as e:
+  print("[-] An error is happening when accessing the URL:%s\n    %s" % (url, e), file=sys.stderr)
   exit()
   
-if result.status_code >= 400:
-  print("An error is happening when accessing the URL:%s\nStatus code is %s." % (url, result.status_code ))
+if response.status_code >= 400:
+  print("[-] An error is happening when accessing the URL:%s\n    Status code is %s." % (url, response.status_code), file=sys.stderr)
   exit()
-  
-contents=result.text
+
+contents=response.text
 
 #3.Extract the comment
 #html comment <!--  -->
+print("[+] Extracting comments")
 htmlcomments=re.findall("<!--(.*?)-->", contents, flags=re.DOTALL)
 
 #js <script > </script> /* */, //
 javascripts=re.findall("<script[^>]*>(.*?)</script>", contents, flags=re.DOTALL)
-
 jscomments=[]
 for javascript in javascripts:
   jscomments += re.findall("/\*(.*?)\*/", javascript, flags=re.DOTALL)
@@ -41,16 +82,7 @@ csscomments=[];
 for css in csss:
   csscomments+=re.findall("/\*(.*?)\*/", css, flags=re.DOTALL)
 
-
 #4.Print the comment
-if htmlcomments:
-  for htmlcomment in htmlcomments:
-    print("[HTML]: " + htmlcomment.strip())
-
-if jscomments:
-  for jscomment in jscomments:
-    print("[JS  ]: " + jscomment.strip())
-
-if csscomments:  
-  for csscomment in csscomments:
-    print("[CSS ]: " + csscomment.strip())
+output("HTML", htmlcomments)
+output("JS", jscomments)
+output("CSS", csscomments)
